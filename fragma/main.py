3dbc67595c398640cfb752a525199976933d3828
Flavien Loiseau <flavien.loiseau@ens-paris-saclay.fr>
2024-01-05 10:30:19 +0100

    Re-organize the Elasticity2DSolver class

--- fragma/main.py
+++ fragma/main.py
@@ -1,3 +1,5 @@
+import tomllib
+
 from models.elasticity import Elasticity2DSolver
 
 # TODO
@@ -7,7 +9,7 @@ from models.elasticity import Elasticity2DSolver
 #       Compute and export the strain
 #       Compute and export the stress
 #   Cleaning
-#       Might be able to separate the definition of the energy from the other of the code
+#       Might be able to separate the definition of the energy from the other in the code
 #   Fracture
 #       Add a residual stiffness
 #       Crack phase=1 at crack tip? Or along the whole crack lips?
@@ -28,6 +30,20 @@ Author(s):
 """
 )
 
-# Use the solver
-solver = Elasticity2DSolver()
+# Read the parameter file
+with open("parameters.toml", "rb") as toml_file:
+    pars = tomllib.load(toml_file)
+
+# Choose the solver
+model = pars["model"]["name"]
+match model:
+    case "elasticity":
+        if pars["model"]["dim"] == 2:
+            solver = Elasticity2DSolver(pars)
+        else:
+            raise NotImplementedError(f"Solver for 3D elasticity is not implemented.")
+    case _:
+        raise NotImplementedError(f"Model '{model}' is not implemented.")
+
+# Run the solver
 solver.solve()
