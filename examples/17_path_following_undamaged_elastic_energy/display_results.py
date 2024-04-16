cb4072eef18b37db21421e2a4f43c3121cdcf844
Flavien Loiseau <flavien.loiseau@ensta-paris.fr>
2024-04-16 10:39:00 +0200

    Autoformat with black

--- examples/17_path_following_undamaged_elastic_energy/display_results.py
+++ examples/17_path_following_undamaged_elastic_energy/display_results.py
@@ -13,7 +13,7 @@ u1_bot = df["displacement 1 [0, 0.017, 0]"]
 u2_bot = df["displacement 2 [0, 0.017, 0]"]
 u1_top = df["displacement 1 [0, 0.02115, 0]"]
 u2_top = df["displacement 2 [0, 0.02115, 0]"]
-u_extenso = np.sqrt((u1_top-u1_bot)**2 + (u2_top-u2_bot)**2)
+u_extenso = np.sqrt((u1_top - u1_bot) ** 2 + (u2_top - u2_bot) ** 2)
 f_bot = -df["F_2 (bot_pin)"] * B
 
 
@@ -33,5 +33,3 @@ plt.grid()
 
 # Show the figures
 plt.show()
-         
-
