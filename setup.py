e994f73aec0fdc10ee28b26211210992fe158e4d
Flavien Loiseau <flavien.loiseau@ensta-paris.fr>
2025-11-28 17:12:50 +0100

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,33 @@
+import datetime
+import setuptools
+
+version = datetime.date.today().strftime("%Y.%m.%d")
+
+setuptools.setup(
+    name="fragma",
+    version=version,
+    author="Flavien Loiseau",
+    author_email="flavien.loiseau@ensta.fr",
+    description=open("README.md", "r").readlines()[1][:-1],
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/floiseau/fragma",
+    packages=["fragma"],
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pyvista",
+        "gmsh",
+        "sympy",
+        "scipy",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fragma = fragma:fragma",
+        ]
+    },
+)
